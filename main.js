c5d84685b2f069ee55c1d16a60bb5a1c9f1a7fbe
ItsJustMe41 <65431481+Olagunju9148@users.noreply.github.com>
2023-06-03 15:59:35 +0100

    New update

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 let outputIncrement = document.getElementById("output")
 let outputDecrement = document.getElementById("output")
 let outputReset = document.getElementById("output")
-console.log(outputIncrement);
 
 let output = 0
-console.log(output);
 
 function increment() {
     output += 1
     outputIncrement.innerText = output
-    console.log(output);
 }
 
 function decrement() {
     output -= 1 
     outputDecrement.innerText = output
-    console.log(output);
+
+    if (output <= 0) {
+        output = 0
+        outputDecrement.textContent = output
+    }
 }
 
 function reset() {
     output = 0
     outputReset.innerText = output
-    console.log(output);
 }
+
